9fc0f32a9e215ed1f8763aed76ac5256741920b3
YunJae Park <zerglisk123@naver.com>
2023-01-10 00:27:48 +0900

    create

---
+++ src/main/java/gutsandgun/kite_sendmsg/KiTeSendMsgApplication.java
@@ -0,0 +1,13 @@
+package gutsandgun.kite_sendmsg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KiTeSendMsgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KiTeSendMsgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gutsandgun/kite_sendmsg/KiTeSendMsgApplicationTests.java
@@ -0,0 +1,13 @@
+package gutsandgun.kite_sendmsg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KiTeSendMsgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
